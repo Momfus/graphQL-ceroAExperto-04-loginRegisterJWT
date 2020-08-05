11d8204a097f39091cb177fbc916493db69ede8b
Momfus <momfus@outlook.com>
2020-08-05 01:43:21 -0300

    añadido campo de email y password con bootstra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { GraphqlModule } from './graphql/graphql.module';
+import { LoginComponent } from './components/login/login.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GraphqlModule
+    GraphqlModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: any = {
+    email: '',
+    password: ''
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  save(): void {
+
+    console.log( this.user );
+
+  }
+
+}
